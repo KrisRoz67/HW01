fb29198705a4a10934dd6c218520308c15726c5b
Kristina Rozanova <zolina.kristina@gmail.com>
2023-09-18 12:35:29 +0300

    Greeting logic added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String username = scanner.nextLine();
+        System.out.println("Welcome," + username + "!");
     }
 
 }
